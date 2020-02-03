14ae0784f9251d1b98a1fef78aa4db47cbe963a0
John Souza <souzajohnlee@gmail.com>
2020-02-03 20:21:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ end
 def map_to_square(source_array)
   i = 0
   source_array.each do |number|
-    number = number ** 2
+    source_array[number] = source_array[number] ** 2
   end
   source_array
 end
